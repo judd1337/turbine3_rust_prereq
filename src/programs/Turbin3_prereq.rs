5ade2f6dcb52a846a1fe077a44a7ee3a6375b121
David Lindstrom <david.lindstroem@gmail.com>
2025-01-10 08:45:48 +0100

    new program address

--- src/programs/Turbin3_prereq.rs
+++ src/programs/Turbin3_prereq.rs
@@ -2,7 +2,7 @@ use solana_idlgen::idlgen; idlgen!({
     "version": "0.1.0",
     "name": "wba_prereq",
     "metadata": {
-        "address": "HC2oqz2p6DEWfrahenqdq2moUcga9c9biqRBcdK3XKU1" 
+        "address": "ADcaide4vBtKuyZQqdU689YqEGZMCmS4tL35bdTv9wJa" 
     },
     "instructions": [
         {
